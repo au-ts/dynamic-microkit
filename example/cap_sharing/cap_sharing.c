f37d45b0c8f09e45b1e4271323a0460d4bc1b20e
Guangtao Zhu <guangtaozhu@outlook.com>
2026-08-06 15:35:22 +1000

    cap_sharing: test cap transfer
    Signed-off-by: Guangtao Zhu <guangtaozhu@outlook.com>

--- example/cap_sharing/cap_sharing.c
+++ example/cap_sharing/cap_sharing.c
@@ -13,6 +13,8 @@
 #define CAP_SECONDARY_TCB (microkit_cspace_root_slot_to_cptr(2))
 #define CAP_MY_SC         (microkit_cspace_root_slot_to_cptr(3))
 #define CAP_MY_TCB        (microkit_cspace_root_slot_to_cptr(4))
+#define CAP_SHARING_CND   (microkit_cspace_root_slot_to_cptr(5))
+#define CAP_SECONDARY_CSP (microkit_cspace_root_slot_to_cptr(6))
 
 static void halt(void)
 {
@@ -54,10 +56,57 @@ void init(void)
         halt();
     }
 
+    microkit_dbg_puts("|primary  | begin test for cap sharing...\n");
+    /* try to send signal to the secondary PD... */
+    microkit_notify(CH_SECONDARY);
+#if 0
     microkit_dbg_puts("|primary  | halting (success)...\n");
     halt();
+#endif
 }
 
 void notified(microkit_channel ch)
 {
+    microkit_dbg_puts("=> primary$ notified by secondary!\n");
+
+    microkit_dbg_puts("    |primary  | ready to stop secondary from sending signals\n");
+    microkit_dbg_puts("    |primary  | secondary should prints null cap invocation\n");
+    /* once receive a signal back, stop the secondary PD from sending signals */
+    seL4_Error err = seL4_CNode_Move(
+        CAP_SHARING_CND,
+        3, /* some random available slot in the sharing CNode */
+        (64 - PD_ROOT_CAP_BITS),
+        CAP_SECONDARY_CSP,
+        BASE_OUTPUT_NOTIFICATION_CAP + CH_SECONDARY,
+        (64 - PD_ROOT_CAP_BITS)
+    );
+    if (err != seL4_NoError) {
+        microkit_dbg_puts("|primary | error moving secondary's channel 0 cap to sharing CNode\n");
+        halt();
+    }
+    /* we can still send signals,
+     * but secondary will never be able to signal back. */
+    for (int i = 0; i < 4; ++i) {
+        /* primary has lower priority, so it will not block the secondary. */
+        microkit_notify(CH_SECONDARY);
+    }
+
+    microkit_dbg_puts("    |primary  | ready to move back the ntfn cap\n");
+
+    /* move back the ntfn cap for the secondary PD to notify back again */
+    err = seL4_CNode_Move(
+        CAP_SECONDARY_CSP,
+        BASE_OUTPUT_NOTIFICATION_CAP + CH_SECONDARY,
+        (64 - PD_ROOT_CAP_BITS),
+        CAP_SHARING_CND,
+        3, /* some random available slot in the sharing CNode */
+        (64 - PD_ROOT_CAP_BITS)
+    );
+    if (err != seL4_NoError) {
+        microkit_dbg_puts("|primary | error moving secondary's channel 0 cap back from sharing CNode\n");
+        halt();
+    }
+
+    microkit_dbg_puts("    |primary  | at this point, primary can hear back from secondary again!\n");
+    microkit_notify(CH_SECONDARY);
 }
--- example/cap_sharing/secondary.c
+++ example/cap_sharing/secondary.c
@@ -6,13 +6,25 @@
 #include <stdint.h>
 #include <microkit.h>
 
+static int recv_cnt;
+
 void init(void)
 {
     microkit_dbg_puts("|secondary| hello, world\n");
+    recv_cnt = 9;
 }
 
 void notified(microkit_channel ch)
 {
-    microkit_dbg_puts("|secondary| notified\n");
+    microkit_dbg_puts("|secondary| notified, recv cnt: '");
+    microkit_dbg_put32(recv_cnt);
+    microkit_dbg_puts("'\n");
+
+    recv_cnt--;
+
     microkit_notify(ch);
+
+    if (!recv_cnt) {
+        while (1);
+    }
 }
